0e28a65cbcbd5c125d58330c33876e557bbb455c
lucas229 <lucasro229@gmail.com>
2020-12-03 00:32:33 -0300

    Criando o menu

---
+++ inc/InterfacePrincipal.hpp
@@ -0,0 +1,13 @@
+#ifndef INTERFACEPRINCIPAL_HPP
+#define INTERFACEPRINCIPAL_HPP
+
+#include <iostream>
+
+using namespace std;
+
+class InterfacePrincipal {
+public:
+    void menuPrincipal();
+};
+
+#endif
---
+++ src/InterfacePrincipal.cpp
@@ -0,0 +1,13 @@
+#include "InterfacePrincipal.hpp"
+
+void InterfacePrincipal::menuPrincipal() {
+    cout << "(1) Iniciar nova tabela" << endl;
+    cout << "(2) Cadastrar nova moeda" << endl;
+    cout << "(3) Deletar moeda" << endl;
+    cout << "(4) Alterar valor de conversão" << endl;
+    cout << "(5) Ver tabela de conversão" << endl;
+    cout << "(6) Carregar exemplo" << endl;
+    cout << "(7) Carregar tabela" << endl;
+    cout << "(8) Salvar tabela" << endl;
+    cout << "(0) Finalizar" << endl;
+}
